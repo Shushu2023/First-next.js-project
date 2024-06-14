8e99a3b56ab8d39a8aab33e75bdac28f5c5d2791
snimeri <snimeri0@email.cpcc.edu>
2024-06-14 19:37:32 -0400

    update search

---
+++ app/dashboard/(overview)/loading.tsx
@@ -0,0 +1,7 @@
+//a placeholder (or fallback) to indicate to users that the content is loading. 
+// shows a loading skeleton
+
+import DashboardSkeleton from '@/app/ui/skeletons';
+export default function Loading() {
+    return <DashboardSkeleton />;
+  }
