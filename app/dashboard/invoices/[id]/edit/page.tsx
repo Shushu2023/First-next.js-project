f79df272a6d9d9238ea8208a0d9405596668323d
snimeri <snimeri0@email.cpcc.edu>
2024-06-20 23:33:08 -0400

    added dynamic route for id

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -1,8 +1,14 @@
 import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
-import { fetchCustomers } from '@/app/lib/data';
+import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
  
-export default async function Page() {
+//page components accept a prop called params which you can use to access the id
+export default async function Page({ params }: { params: { id: string } }) {
+  const id = params.id;
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
   return (
     <main>
       <Breadcrumbs
